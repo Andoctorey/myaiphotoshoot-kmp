4968196afa64982a8e513087177440ff8274edb6
andrey <a.d.yegorov@gmail.com>
2024-11-30 11:45:25 +0700

    Min size for desktop

--- composeApp/src/commonMain/kotlin/ai/create/photo/ui/main/MainApp.kt
+++ composeApp/src/commonMain/kotlin/ai/create/photo/ui/main/MainApp.kt
@@ -3,15 +3,11 @@ package ai.create.photo.ui.main
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,8 +22,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.resources.stringResource
-import photocreateai.composeapp.generated.resources.Res
-import photocreateai.composeapp.generated.resources.back_button
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -40,34 +34,9 @@ fun MainApp(
         backStackEntry?.destination?.route ?: AppNavigationRoutes.TAB1.name
     )
 
-    @Composable
-    fun TopAppBar(
-        currentScreen: AppNavigationRoutes,
-        canNavigateBack: Boolean,
-        navigateUp: () -> Unit,
-        modifier: Modifier = Modifier,
-    ) {
-        TopAppBar(
-            title = { Text(stringResource(currentScreen.label)) },
-            colors = TopAppBarDefaults.mediumTopAppBarColors(
-                containerColor = MaterialTheme.colorScheme.primaryContainer
-            ),
-            modifier = modifier,
-            navigationIcon = {
-                if (canNavigateBack) {
-                    IconButton(onClick = navigateUp) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                            contentDescription = stringResource(Res.string.back_button)
-                        )
-                    }
-                }
-            }
-        )
-    }
-
     var currentDestination by rememberSaveable { mutableStateOf(AppNavigationRoutes.TAB1) }
     NavigationSuiteScaffold(
+        modifier = Modifier.widthIn(min = 200.dp),
         navigationSuiteItems = {
             AppNavigationRoutes.entries.forEach {
                 item(
--- composeApp/src/desktopMain/kotlin/ai/create/photo/main.kt
+++ composeApp/src/desktopMain/kotlin/ai/create/photo/main.kt
@@ -1,13 +1,31 @@
 package ai.create.photo
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.FrameWindowScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
         title = "PhotoCreateAi",
     ) {
+        SetMinimumWindowSize(width = 100.dp, height = 200.dp)
         App()
+    }
+}
+
+@Composable
+fun FrameWindowScope.SetMinimumWindowSize(width: Dp, height: Dp) {
+    val density = LocalDensity.current
+    LaunchedEffect(density) {
+        window.minimumSize = with(density) {
+            Dimension(width.toPx().toInt(), height.toPx().toInt())
+        }
     }
 }
