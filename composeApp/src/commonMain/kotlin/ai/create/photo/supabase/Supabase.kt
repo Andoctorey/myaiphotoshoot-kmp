b1f48e79a15b69fc0b0c4da93319b60c588ce986
andrey <a.d.yegorov@gmail.com>
2024-12-25 18:01:25 +0700

    Remove debug config

--- composeApp/src/commonMain/kotlin/ai/create/photo/supabase/Supabase.kt
+++ composeApp/src/commonMain/kotlin/ai/create/photo/supabase/Supabase.kt
@@ -1,8 +1,5 @@
 package ai.create.photo.supabase
 
-import ai.create.photo.platform.Platforms
-import ai.create.photo.platform.platform
-import co.touchlab.kermit.Logger
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.functions.Functions
@@ -15,9 +12,8 @@ import kotlin.time.Duration.Companion.minutes
 
 object Supabase {
 
-    val debug = platform().platform == Platforms.DESKTOP
     val supabase = createSupabaseClient(
-        supabaseUrl = if (debug) "http://127.0.0.1:54321" else "https://trzgfajvyjpvbqedyxug.supabase.co",
+        supabaseUrl = "https://trzgfajvyjpvbqedyxug.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRyemdmYWp2eWpwdmJxZWR5eHVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1NTA5MzAsImV4cCI6MjA0OTEyNjkzMH0.39Qdq2nTCuoIpAfc7L725MZA2ls3NegFy6zCjOTzW9M"
     ) {
         requestTimeout = 2.minutes
@@ -30,5 +26,5 @@ object Supabase {
         install(Storage)
         install(Realtime)
         install(Functions)
-    }.also { if (debug) Logger.w("Using local supabase") }
+    }
 }
