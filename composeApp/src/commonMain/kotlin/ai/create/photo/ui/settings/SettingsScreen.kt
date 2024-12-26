ff4e3d8fb18e98f79ec17d16d1922847b28d1efa
andrey <a.d.yegorov@gmail.com>
2024-12-26 11:56:30 +0700

    ListDetailPaneScaffold

--- composeApp/src/commonMain/kotlin/ai/create/photo/ui/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/ai/create/photo/ui/settings/SettingsScreen.kt
@@ -3,17 +3,31 @@ package ai.create.photo.ui.settings
 import ai.create.photo.ui.compose.ErrorMessagePlaceHolder
 import ai.create.photo.ui.compose.ErrorPopup
 import ai.create.photo.ui.compose.LoadingPlaceholder
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.windowInsetsTopHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
+import androidx.compose.material3.adaptive.layout.AnimatedPane
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffold
+import androidx.compose.material3.adaptive.layout.ListDetailPaneScaffoldRole
+import androidx.compose.material3.adaptive.navigation.rememberListDetailPaneScaffoldNavigator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -35,8 +49,10 @@ fun SettingsScreen(
         } else if (state.loadingError != null) {
             ErrorMessagePlaceHolder(state.loadingError)
         } else {
-            Spacer(Modifier.windowInsetsTopHeight(WindowInsets.systemBars))
-
+            Column {
+                Spacer(Modifier.windowInsetsTopHeight(WindowInsets.systemBars))
+                Screen()
+            }
         }
 
         if (state.errorPopup != null) {
@@ -46,3 +62,99 @@ fun SettingsScreen(
         }
     }
 }
+
+@OptIn(ExperimentalMaterial3AdaptiveApi::class)
+@Composable
+private fun Screen() {
+    val navigator = rememberListDetailPaneScaffoldNavigator<MyItem>()
+
+    ListDetailPaneScaffold(
+        directive = navigator.scaffoldDirective,
+        value = navigator.scaffoldValue,
+        listPane = {
+            AnimatedPane {
+                MyList(
+                    onItemClick = { item ->
+                        // Navigate to the detail pane with the passed item
+                        navigator.navigateTo(ListDetailPaneScaffoldRole.Detail, item)
+                    },
+                )
+            }
+        },
+        detailPane = {
+            AnimatedPane {
+                // Show the detail pane content if selected item is available
+                navigator.currentDestination?.content?.let {
+                    MyDetails(it)
+                }
+            }
+        },
+    )
+}
+
+@Composable
+fun MyList(
+    onItemClick: (MyItem) -> Unit,
+) {
+    Card {
+        LazyColumn {
+            shortStrings.forEachIndexed { id, string ->
+                item {
+                    ListItem(
+                        modifier = Modifier
+                            .clickable {
+                                onItemClick(MyItem(id))
+                            },
+                        headlineContent = {
+                            Text(
+                                text = string,
+                            )
+                        },
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyDetails(item: MyItem) {
+    val text = shortStrings[item.id]
+    Card {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Details page for $text",
+                fontSize = 24.sp,
+            )
+            Spacer(Modifier.size(16.dp))
+            Text(
+                text = "TODO: Add great details here"
+            )
+        }
+    }
+}
+
+
+class MyItem(val id: Int)
+// [END android_compose_adaptivelayouts_sample_list_detail_pane_scaffold_myitem]
+
+val shortStrings = listOf(
+    "Cupcake",
+    "Donut",
+    "Eclair",
+    "Froyo",
+    "Gingerbread",
+    "Honeycomb",
+    "Ice cream sandwich",
+    "Jelly bean",
+    "Kitkat",
+    "Lollipop",
+    "Marshmallow",
+    "Nougat",
+    "Oreo",
+    "Pie",
+)
