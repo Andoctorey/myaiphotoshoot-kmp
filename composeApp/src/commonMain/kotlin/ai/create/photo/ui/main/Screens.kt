1895f7cc0d0285ac2f536f740c5ace3d8db5abe8
andrey <a.d.yegorov@gmail.com>
2025-01-11 09:48:10 +0700

    Brush icon to generate

--- composeApp/src/commonMain/kotlin/ai/create/photo/ui/main/Screens.kt
+++ composeApp/src/commonMain/kotlin/ai/create/photo/ui/main/Screens.kt
@@ -1,7 +1,7 @@
 package ai.create.photo.ui.main
 
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Mood
+import androidx.compose.material.icons.filled.Brush
 import androidx.compose.material.icons.filled.PersonAdd
 import androidx.compose.material.icons.filled.PersonSearch
 import androidx.compose.material.icons.filled.Settings
@@ -14,7 +14,7 @@ enum class AppNavigationRoutes(
     val icon: ImageVector,
 ) {
     TAB1(Res.string.tab_1, Icons.Default.PersonAdd),
-    TAB2(Res.string.tab_2, Icons.Default.Mood),
+    TAB2(Res.string.tab_2, Icons.Default.Brush),
     TAB3(Res.string.tab_3, Icons.Default.PersonSearch),
     TAB4(Res.string.tab_4, Icons.Default.Settings),
 }
