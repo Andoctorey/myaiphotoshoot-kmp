4cbf0f0633c8642a459be00f7d73902c5c23c246
andrey <a.d.yegorov@gmail.com>
2025-02-09 23:14:12 +0700

    Fix wasm publishing makes duplicates

--- release_web.sh
+++ release_web.sh
@@ -1,3 +1,8 @@
+# https://slack-chats.kotlinlang.org/t/26904919/i-run-wasmjsbrowserdistribution-and-there-are-4-wasm-files-s
+./gradlew clean
+rm -rf composeApp/build/dist/wasmJs/productionExecutable
 ./gradlew wasmJsBrowserDistribution
 git push origin HEAD
 wrangler pages deploy composeApp/build/dist/wasmJs/productionExecutable --project-name myaiphotoshoot
+# purge
+# https://dash.cloudflare.com/1e70ec035798594e4af14687e54fc268/myaiphotoshoot.com/caching/configuration
