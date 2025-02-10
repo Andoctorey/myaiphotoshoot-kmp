4380a94c4f90f406436207e04d881874a199ef84
andrey <a.d.yegorov@gmail.com>
2025-02-10 23:57:35 +0700

    Better gallery icon

--- composeApp/src/commonMain/kotlin/ai/create/photo/ui/main/Screens.kt
+++ composeApp/src/commonMain/kotlin/ai/create/photo/ui/main/Screens.kt
@@ -2,7 +2,7 @@ package ai.create.photo.ui.main
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Brush
-import androidx.compose.material.icons.filled.PersonSearch
+import androidx.compose.material.icons.filled.PhotoLibrary
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import org.jetbrains.compose.resources.StringResource
@@ -12,7 +12,7 @@ enum class AppNavigationRoutes(
     val label: StringResource,
     val icon: ImageVector,
 ) {
-    TAB_1_GALLERY(Res.string.tab_gallery, Icons.Default.PersonSearch),
+    TAB_1_GALLERY(Res.string.tab_gallery, Icons.Default.PhotoLibrary),
     TAB_2_GENERATE(Res.string.tab_generate, Icons.Default.Brush),
     TAB_3_SETTINGS(Res.string.tab_settings, Icons.Default.Settings),
 }
