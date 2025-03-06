80f49d4c47291b2136f4a531eca57218413b5feb
andrey <a.d.yegorov@gmail.com>
2025-03-06 16:07:57 +0700

    Try to use versioning

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,7 +43,7 @@ kotlin {
             val rootDirPath = project.rootDir.path
             val projectDirPath = project.projectDir.path
             commonWebpackConfig {
-                outputFileName = "composeApp.js"
+                outputFileName = "composeApp.js?v=2"
                 devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
                     static = (static ?: mutableListOf()).apply {
                         // Serve sources to debug inside browser
