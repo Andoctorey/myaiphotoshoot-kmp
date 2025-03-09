5070aeee76a6eeb250de2920c039dc0d0b09ba45
andrey <a.d.yegorov@gmail.com>
2025-03-09 19:03:25 +0700

    Update deps

--- release_web.sh
+++ release_web.sh
@@ -1,5 +1,6 @@
 # https://youtrack.jetbrains.com/issue/KT-73907/Wasm-Duplication-of-files-in-browser-distribution#focus=Comments-27-11344610.0-0
 ./gradlew clean
+./gradlew clean
 rm -rf composeApp/build/dist/wasmJs/productionExecutable
 ./gradlew wasmJsBrowserDistribution
 wrangler pages deploy composeApp/build/dist/wasmJs/productionExecutable --project-name myaiphotoshoot --commit-dirty=true
