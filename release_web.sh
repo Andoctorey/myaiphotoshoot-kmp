1dd9fd82319a2c6a4806dc80b48ac5293c10b0cd
andrey <a.d.yegorov@gmail.com>
2025-05-16 11:15:29 +0700

    Clean web script

--- release_web.sh
+++ release_web.sh
@@ -1,9 +1,5 @@
 # https://youtrack.jetbrains.com/issue/KT-73907/Wasm-Duplication-of-files-in-browser-distribution#focus=Comments-27-11344610.0-0
 ./gradlew clean
-# doesn't work from first time sometimes
-./gradlew clean
 rm -rf composeApp/build/dist/wasmJs/productionExecutable
 ./gradlew wasmJsBrowserDistribution
 wrangler pages deploy composeApp/build/dist/wasmJs/productionExecutable --project-name myaiphotoshoot --commit-dirty=true
-# purge cache - no need already, keeping for edge cases
-# open https://dash.cloudflare.com/1e70ec035798594e4af14687e54fc268/myaiphotoshoot.com/caching/configuration
