b50cd184bbc7a592c1efcd444dbcddad0a096a4f
andrey <a.d.yegorov@gmail.com>
2025-05-23 10:51:47 +0700

    Fix navigation bar color

--- composeApp/src/androidMain/kotlin/ai/create/photo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/ai/create/photo/MainActivity.kt
@@ -1,19 +1,26 @@
 package ai.create.photo
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.WindowCompat
 import io.github.vinceglb.filekit.core.FileKit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         FileKit.init(this)
-        enableEdgeToEdge()
         setContent {
+            enableEdgeToEdge()
+            UpdateSystemBarsColor()
             App()
         }
     }
@@ -23,4 +30,25 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun AppAndroidPreview() {
     App()
+}
+
+@Composable
+fun UpdateSystemBarsColor() {
+    val view = LocalView.current
+    val context = LocalContext.current
+    val isDarkTheme = isSystemInDarkTheme()
+//    val color = if (isDarkTheme) {
+//        // Define your dark theme navigation bar color
+//        Color.Black
+//    } else {
+//        // Define your light theme navigation bar color
+//        Color.White
+//    }
+
+    SideEffect {
+        val window = (context as Activity).window
+//        window.navigationBarColor = color.toArgb() deprecated, use navigationBarsPadding()
+        WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars =
+            !isDarkTheme
+    }
 }
