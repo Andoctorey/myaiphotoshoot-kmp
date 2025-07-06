dda53b9bac9ca2c66c3a263afd542315dfc932ad
andrey <a.d.yegorov@gmail.com>
2025-07-06 13:05:50 +0700

    Version 1.4 - blog

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -152,7 +152,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         versionCode = gitVersionCode
-        versionName = "1.2.$gitVersionCode"
+        versionName = "1.3.$gitVersionCode"
     }
     packaging {
         resources {
